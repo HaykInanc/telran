0e2bf91c6e4c2e53f8a129f6ca42b4344fca8500
haykInants <hayk.inanc@gmail.com>
2022-06-23 11:50:59 +0400

    init commit

---
+++ script/script.js
@@ -0,0 +1,26 @@
+const user_add_form_elem = document.querySelector('#user_add_form');
+const clearBtn = user_add_form_elem.querySelector('.clear');
+const users = [];
+
+
+user_add_form_elem.addEventListener('submit', event =>{
+    event.preventDefault();
+    const {name, lastname, avatar} = event.target;
+    const user = {
+        name: name.value,
+        lastname: lastname.value,
+        avatar: avatar.value
+    };
+    users.push(user);
+})
+
+clearBtn.addEventListener('click', event =>{
+    event.preventDefault();
+    const {name, lastname, avatar} = user_add_form_elem;
+    name.value = '';
+    lastname.value = '';
+    avatar.value = '';
+})
+
+// при отправке формы в массив должен добавляться
+// объект с описанием пользователя
